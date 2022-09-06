cd758a314b721133c7a2112b7064986ff03bbf20
arupay <arupay11374@gmail.com>
2022-09-06 07:38:17 -0400

    fixed simple maps dependency issue

--- front-end/src/Components/TruckMap.js
+++ front-end/src/Components/TruckMap.js
@@ -1,20 +1,32 @@
-import GoogleMaps from "simple-react-google-maps";
 import { Container } from "react-bootstrap";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 const MAP_API_KEY = process.env.REACT_APP_MAP_API_KEY;
 
 function TruckMap({ latitude, longitude }) {
   const coordinates = { lat: Number(latitude), lng: Number(longitude) };
+  const containerStyle = {
+    width: "100%",
+    height: "50vh",
+  };
 
   return (
-    <Container className="mt-1 mb-1">
-      {typeof coordinates.lat === "number" && (
-        <GoogleMaps
-          apiKey={MAP_API_KEY}
-          style={{ height: "400px", width: "100%" }}
-          zoom={15}
-          center={coordinates}
-          markers={coordinates} //optional
-        />
+    <Container>
+      {coordinates.lat && (
+        <LoadScript googleMapsApiKey={MAP_API_KEY}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={coordinates}
+            zoom={15}
+          >
+            <MarkerF
+              position={coordinates}
+              icon={{
+                // url: "https://gcdnb.pbrd.co/images/a8B831n9dfxr.jpg",
+                url: "https://gcdnb.pbrd.co/images/UPbgm8xNJfhS.png",
+              }}
+            />
+          </GoogleMap>
+        </LoadScript>
       )}
     </Container>
   );
